3018cc0d019f9c81c01f4e218251f7d43c46a8e6
SR <saodat.riskiyeva@gmail.com>
2025-09-15 19:04:39 -0400

    creating database pool

---
+++ data/database.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  database: "blog",
+  user: "root",
+  password: "mySQL7!",
+});
+
+module.exports = pool;
